f99d4dd8aeb2398c865f79e922491d7810d40d85
tommasocostanzo99 <tommasocostanzo99@gmail.com>
2018-02-14 05:44:10 -0500

    htm

---
+++ index.php
@@ -0,0 +1,21 @@
+<html>
+  <head>
+  
+  </head>
+  <body>
+    <form method="post">
+        <p>Nome: </p><input type="text" name="nome">
+        <p>Cognome: </p><input type="text" name="Cognome">
+        <p>Sesso: </p><input type="radio" name="sesso" value="Maschio">Maschio <input type="radio" name="sesso" value="Femmina"> Femmina
+        <p>Nazionalit&agrave:</p>  
+        <select name="nazionalita">
+          <option value="italiana">Italiana</option>
+          <option value="tedesca">Tedesca</option>
+          <option value="francese"></option>
+        </select>
+        <p>Patente: </p><input type="checkbox" name="patentea" value="a">Cat. a <input type="checkbox" name="patenteb" value="b"> Cat. b<br>
+       <p>Email: </p><input type="text" name="email">
+      <p>Password: </p><input type="password" name="password">
+    </form>
+  </body>
+</html>
