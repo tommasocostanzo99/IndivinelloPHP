9033b33d1c411f629fd79984ca3346dc6ca65261
tommasocostanzo99 <tommasocostanzo99@gmail.com>
2018-02-15 04:47:57 -0500

    addes jsScript

--- index.php
+++ index.php
@@ -1,21 +1,66 @@
 <html>
   <head>
-  
+    <script>
+      function confermaModulo(){
+        for(int i=0;i<document.getElementsByName("formRegistrazione").length; i++){
+            if(document.getElementsByName("formRegistrazione")[i].value==null){
+               alert("non hai inserito tutti i campi");
+               return;
+        }
+        if(document.getElementsByName("sesso")[0].checked==false && document.getElementsByName("sesso")[1].checked==false){
+            alert("non hai inserito tutti i campi");
+        }
+        /*if(document.getElementsByName("nome")[0].value==null){
+           alert("non hai inserito tutti i campi");
+            
+        }
+        else if(document.getElementsByName("cognome")[0].value==null){
+           alert("non hai inserito tutti i campi");
+           
+        }
+        else 
+        }
+        else if(document.getElementsByName("email")[0].value==null){
+            alert("non hai inserito tutti i campi");
+        }
+        else if(document.getElementsByName("password")[0].value==null){
+            alert("non hai inserito tutti i campi");
+        }*/
+        else{
+            document.getElementById("formRegistrazione").style.display="none";
+            document.getElementById("confermaModulo").style.display="block";
+            document.getElementsByName("riepilogo")[0].innerHt
+            
+            
+        }
+      }
+    
+    </script>
   </head>
   <body>
-    <form method="post">
-        <p>Nome: </p><input type="text" name="nome">
-        <p>Cognome: </p><input type="text" name="Cognome">
+    <form id="formRegistrazione">
+        <p>Nome: </p><input type="text" name="formRegistrazione">
+        <p>Cognome: </p><input type="text" name="formRegistrazione">
         <p>Sesso: </p><input type="radio" name="sesso" value="Maschio">Maschio <input type="radio" name="sesso" value="Femmina"> Femmina
         <p>Nazionalit&agrave:</p>  
-        <select name="nazionalita">
+        <select id="nazionalita" name="formRegistrazione">
           <option value="italiana">Italiana</option>
           <option value="tedesca">Tedesca</option>
-          <option value="francese"></option>
+          <option value="francese">Francese</option>
         </select>
-        <p>Patente: </p><input type="checkbox" name="patentea" value="a">Cat. a <input type="checkbox" name="patenteb" value="b"> Cat. b<br>
-       <p>Email: </p><input type="text" name="email">
-      <p>Password: </p><input type="password" name="password">
+        <p>Patente: </p><input type="checkbox" name="patente" value="a">Cat. a <input type="checkbox" name="patente" value="b"> Cat. b<br>
+       <p>Email: </p><input type="text" name="formRegistrazione">
+      <p>Password: </p><input type="password" name="formRegistrazione">
+      <input type="button" name="conferma" onclick="confermaModulo()" value="Conferma">
+
     </form>
+    <div id="confermaModulo" style="display:none;" >
+        <p name="riepilogo">Nome: </p>
+        <p name="riepilogo">Cognome: </p>
+        <p name="riepilogo">Sesso: </p>
+        <p name="riepilogo">Nazionalit&agrave: </p>
+        <p name="riepilogo">Patente: </p>
+        <p name="riepilogo">Email: </p>
+    </div>
   </body>
 </html>
